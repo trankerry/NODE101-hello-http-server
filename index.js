2831f9366df63f23f39e3a80187500aa46c25b7b
Kerry <trankerry94@gmail.com>
2021-11-08 10:25:40 -0800

    finished project

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // write your code here
+const http  = require('http');
+
+const requestListener = function (req, res) {
+    res.writeHead(200);
+    res.end('Hello World');
+}
+
+const server = http.createServer(requestListener);
+server.listen(8080)
